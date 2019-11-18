6e837e90b6f1d3bf77da02a9ab2b92c9f862607b
Martin Weiss <albusmw@gmx.de>
2019-11-18 10:09:58 +0100

    WebServer project checkin

---
+++ WebServer/WebServer/Main.vb
@@ -0,0 +1,12 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class Main
+
+    Public Shared Sub Main()
+        Dim ws As WebServer = WebServer.getWebServer
+        ws.VirtualRoot() = "C:/webserver_root/"
+        ws.StartWebServer()
+    End Sub
+
+End Class
