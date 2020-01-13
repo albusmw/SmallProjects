094d270b0b5994f74ceef9d292998d0f5e1cfa5b
Martin Weiss <albusmw@gmx.de>
2020-01-13 10:35:44 +0100

    added SBahnMonitor project

---
+++ SBahnMonitor/SBahnMonitor/SBahnMonitor/MainForm.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.wbMain = New System.Windows.Forms.WebBrowser()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.scMain = New System.Windows.Forms.SplitContainer()
+        Me.tbLog = New System.Windows.Forms.TextBox()
+        Me.DebugToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StoreAllEVAIDsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CheckAllTrainsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.scMain, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.scMain.Panel1.SuspendLayout()
+        Me.scMain.Panel2.SuspendLayout()
+        Me.scMain.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'wbMain
+        '
+        Me.wbMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.wbMain.Location = New System.Drawing.Point(0, 0)
+        Me.wbMain.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.wbMain.Name = "wbMain"
+        Me.wbMain.ScriptErrorsSuppressed = True
+        Me.wbMain.Size = New System.Drawing.Size(728, 1181)
+        Me.wbMain.TabIndex = 0
+        Me.wbMain.Url = New System.Uri("", System.UriKind.Relative)
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.GripMargin = New System.Windows.Forms.Padding(2, 2, 0, 2)
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.DebugToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1450, 33)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StartToolStripMenuItem, Me.CheckAllTrainsToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(54, 29)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'StartToolStripMenuItem
+        '
+        Me.StartToolStripMenuItem.Name = "StartToolStripMenuItem"
+        Me.StartToolStripMenuItem.Size = New System.Drawing.Size(270, 34)
+        Me.StartToolStripMenuItem.Text = "Start"
+        '
+        'scMain
+        '
+        Me.scMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.scMain.Location = New System.Drawing.Point(0, 33)
+        Me.scMain.Name = "scMain"
+        '
+        'scMain.Panel1
+        '
+        Me.scMain.Panel1.Controls.Add(Me.wbMain)
+        '
+        'scMain.Panel2
+        '
+        Me.scMain.Panel2.Controls.Add(Me.tbLog)
+        Me.scMain.Size = New System.Drawing.Size(1450, 1181)
+        Me.scMain.SplitterDistance = 728
+        Me.scMain.TabIndex = 2
+        '
+        'tbLog
+        '
+        Me.tbLog.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tbLog.Font = New System.Drawing.Font("Courier New", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbLog.Location = New System.Drawing.Point(0, 0)
+        Me.tbLog.Multiline = True
+        Me.tbLog.Name = "tbLog"
+        Me.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.tbLog.Size = New System.Drawing.Size(718, 1181)
+        Me.tbLog.TabIndex = 0
+        '
+        'DebugToolStripMenuItem
+        '
+        Me.DebugToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StoreAllEVAIDsToolStripMenuItem})
+        Me.DebugToolStripMenuItem.Name = "DebugToolStripMenuItem"
+        Me.DebugToolStripMenuItem.Size = New System.Drawing.Size(82, 29)
+        Me.DebugToolStripMenuItem.Text = "Debug"
+        '
+        'StoreAllEVAIDsToolStripMenuItem
+        '
+        Me.StoreAllEVAIDsToolStripMenuItem.Name = "StoreAllEVAIDsToolStripMenuItem"
+        Me.StoreAllEVAIDsToolStripMenuItem.Size = New System.Drawing.Size(270, 34)
+        Me.StoreAllEVAIDsToolStripMenuItem.Text = "Store all EVA ID's"
+        '
+        'CheckAllTrainsToolStripMenuItem
+        '
+        Me.CheckAllTrainsToolStripMenuItem.Name = "CheckAllTrainsToolStripMenuItem"
+        Me.CheckAllTrainsToolStripMenuItem.Size = New System.Drawing.Size(270, 34)
+        Me.CheckAllTrainsToolStripMenuItem.Text = "Check all trains"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1450, 1214)
+        Me.Controls.Add(Me.scMain)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "MainForm"
+        Me.Text = "MainForm"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.scMain.Panel1.ResumeLayout(False)
+        Me.scMain.Panel2.ResumeLayout(False)
+        Me.scMain.Panel2.PerformLayout()
+        CType(Me.scMain, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.scMain.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents wbMain As WebBrowser
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StartToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents scMain As SplitContainer
+    Friend WithEvents tbLog As TextBox
+    Friend WithEvents CheckAllTrainsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DebugToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StoreAllEVAIDsToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ SBahnMonitor/SBahnMonitor/SBahnMonitor/MainForm.vb
@@ -0,0 +1,159 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class MainForm
+
+    Dim MyPath As String = System.Reflection.Assembly.GetExecutingAssembly.Location
+
+    Dim Downloader As New AstroCalc.NET.Common.cDownloader
+    Dim EvaIDs As New Dictionary(Of String, String)              'Dictionary of EVA ID's and station names
+    Dim AllTrains As New List(Of String)                         'Lik to all trains
+
+    Private Sub StartToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles StartToolStripMenuItem.Click
+
+        'https://christoph-stoeber.de/bahnprojekt/dokumentation/bahndetafeldoku.php
+
+        'Mit infos:
+        'https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?ld=37235&protocol=https:&rt=1&
+
+        'delayedJourney=on                                  Zeigt nur verspätete oder ausfallende Züge an. 
+
+        Dim Language As String = "d"
+        Dim Darstellung As String = "n"                     'n: Standard / l: nur Text OHNE ECHTZEIT
+        Dim QueryDate As String = Format(Now, "dd.MM.yy")   ' "12.01.20"
+        Dim QueryTime As String = Format(Now, "hh:mm")      '"12:00"
+        Dim Station As String = "8004132"                   'Stachus
+        Dim maxJourneys As String = "50"                    'Anzahl angezeigter Züge
+        Dim BoardType As String = "dep"                     'arr/dep
+
+        Dim StartURL As String = "https://reiseauskunft.bahn.de/bin/bhftafel.exe/" & Language & Darstellung & "?ld=37235&country=DEU&protocol=https:&rt=1&input=" & Station & "&boardType=" & BoardType & "&time=" & QueryTime & "&productsFilter=11111&&&date=" & QueryDate & "&selectDate=&maxJourneys=" & maxJourneys & "&start=yes"
+
+        'Initial scan for start station
+        Navigate(StartURL)
+
+        Dim LinkCount As Integer = 0
+        Dim NewTrainLinks As Integer = 0
+        AddLog(wbMain.Document.Links.Count.ToString.Trim & " links")
+        For Each Element As HtmlElement In wbMain.Document.Links
+            'Get link and filter
+            Dim TrainInfoLink As String = Element.GetAttribute("href")
+            If TrainInfoLink.StartsWith("https://reiseauskunft.bahn.de/bin/traininfo.exe") Then
+                LinkCount += 1
+                'Get the unique link for this train for the current day
+                '1.) Remove ld
+                Dim ldPos As Integer = TrainInfoLink.IndexOf("ld=")
+                If ldPos > 0 Then
+                    TrainInfoLink = TrainInfoLink.Substring(0, ldPos) & TrainInfoLink.Substring(TrainInfoLink.IndexOf("&", ldPos) + 1)
+                End If
+                '2.) Remove time
+                Dim timePos As Integer = TrainInfoLink.IndexOf("time=")
+                If timePos > 0 Then
+                    TrainInfoLink = TrainInfoLink.Substring(0, timePos) & TrainInfoLink.Substring(TrainInfoLink.IndexOf("&", timePos) + 1)
+                End If
+                '2.) Remove station
+                Dim stationPos As Integer = TrainInfoLink.IndexOf("station_evaId=")
+                If stationPos > 0 Then
+                    TrainInfoLink = TrainInfoLink.Substring(0, stationPos) & TrainInfoLink.Substring(TrainInfoLink.IndexOf("&", stationPos) + 1)
+                End If
+                If AllTrains.Contains(TrainInfoLink) = False Then
+                    NewTrainLinks += 1
+                    AllTrains.Add(TrainInfoLink)
+                End If
+            End If
+        Next Element
+        AddLog("  > " & LinkCount.ToString.Trim & " trains found")
+        AddLog("  > " & NewTrainLinks.ToString.Trim & " new trains found")
+
+        'Get all trains present
+        CheckAllTrains(AllTrains)
+        AddLog("=====================================================================")
+
+    End Sub
+
+    Private Sub CheckAllTrains(ByVal TrainList As List(Of String))
+        Dim NewEVAIDCount As Integer = 0
+        For Each Train As String In TrainList
+
+            Dim TrainDepOnTime As Integer = 0
+            Dim TrainDepDelayedOnTime As Integer = 0
+            Dim TrainDepDelayed As Integer = 0
+            Navigate(Train)
+
+            'Check all links
+            For Each Element As HtmlElement In wbMain.Document.Links
+                'Get link and filter
+                Dim InnerLink As String = Element.GetAttribute("href")
+                If InnerLink.StartsWith("https://reiseauskunft.bahn.de/bin/bhftafel.exe") Then
+                    Dim BahnhofName As String = Element.InnerText.Trim              'Text as displayed
+                    Dim EvaID As String = InnerLink.PartFrom("input=")
+                    EvaID = EvaID.Substring(EvaID.IndexOf("%") + 3)
+                    EvaID = EvaID.Substring(0, EvaID.IndexOf("&"))
+                    If EvaIDs.ContainsKey(EvaID) = False Then
+                        EvaIDs.Add(EvaID, BahnhofName)
+                        NewEVAIDCount += 1
+                    End If
+                End If
+            Next Element
+
+            'Check all departure elements
+            Dim AllElements As HtmlElementCollection = wbMain.Document.GetElementsByTagName("td")
+            For Each Elements As HtmlElement In AllElements
+                If IsNothing(Elements.InnerText) = False Then
+                    If Elements.InnerText.Contains("ab ") Then
+                        If Elements.InnerText = Elements.InnerHtml Then
+                            'Both are the same -> no additional information ...
+                            TrainDepOnTime += 1
+                        Else
+                            If Elements.InnerHtml.Contains("class=delayOnTime") Then
+                                TrainDepDelayedOnTime += 1
+                            Else
+                                If Elements.InnerHtml.Contains("class=delay") Then
+                                    TrainDepDelayed += 1
+                                End If
+                            End If
+                        End If
+                    End If
+                End If
+            Next Elements
+            Dim TotalDeps As Integer = TrainDepOnTime + TrainDepDelayed + TrainDepDelayedOnTime
+
+            If TrainDepDelayed + TrainDepDelayedOnTime > 0 Then
+                AddLog("  > " & Train)
+                AddLog("  > " & TotalDeps.ToString.Trim & " departures, " & TrainDepDelayedOnTime.ToString.Trim & " delayed on time, " & TrainDepDelayed.ToString.Trim & " delayed")
+            End If
+        Next Train
+        If NewEVAIDCount > 0 Then AddLog("  > " & NewEVAIDCount.ToString.Trim & " new EVA ID's found")
+    End Sub
+
+    Private Sub Navigate(ByVal URL As String)
+        wbMain.ScriptErrorsSuppressed = True
+        wbMain.Navigate(URL)
+        Do
+            System.Windows.Forms.Application.DoEvents()
+        Loop Until wbMain.IsBusy = False
+        'MsgBox("OK")
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Me.Text = "SBahnMonitor (" & MyPath & ")"
+    End Sub
+
+    Private Sub AddLog(ByVal Text As String)
+        tbLog.Text &= Text & System.Environment.NewLine
+        System.Windows.Forms.Application.DoEvents()
+    End Sub
+
+    Private Sub StoreAllEVAIDsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles StoreAllEVAIDsToolStripMenuItem.Click
+        'Store all stations
+        Dim AllStations As New List(Of String)
+        For Each EvaID As String In EvaIDs.Keys
+            AllStations.Add(EvaID.Trim & ";" & EvaIDs(EvaID))
+        Next EvaID
+        System.IO.File.WriteAllLines("AllStations.csv", AllStations.ToArray, System.Text.Encoding.Unicode)
+    End Sub
+
+    Private Sub CheckAllTrainsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CheckAllTrainsToolStripMenuItem.Click
+        CheckAllTrains(AllTrains)
+    End Sub
+
+End Class
