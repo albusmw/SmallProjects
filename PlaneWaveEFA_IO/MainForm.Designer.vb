cd02173ab2009551a6a1dce1827af33cb830fbf9
albusmw <albusmw@gmx.de>
2020-02-14 22:57:19 +0100

    updated to PlaneWaveEFA_IO

--- PlaneWaveEFA_IO/MainForm.Designer.vb
+++ PlaneWaveEFA_IO/MainForm.Designer.vb
@@ -22,62 +22,71 @@ Partial Class MainForm
   'Do not modify it using the code editor.
   <System.Diagnostics.DebuggerStepThrough()> _
   Private Sub InitializeComponent()
-    Me.btnGo = New System.Windows.Forms.Button()
-    Me.lbLog = New System.Windows.Forms.ListBox()
-    Me.Button1 = New System.Windows.Forms.Button()
-    Me.SuspendLayout()
-    '
-    'btnGo
-    '
-    Me.btnGo.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.lbLog = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.pgMain = New System.Windows.Forms.PropertyGrid()
+        Me.SuspendLayout()
+        '
+        'btnGo
+        '
+        Me.btnGo.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-    Me.btnGo.Location = New System.Drawing.Point(12, 12)
-    Me.btnGo.Name = "btnGo"
-    Me.btnGo.Size = New System.Drawing.Size(319, 36)
-    Me.btnGo.TabIndex = 0
-    Me.btnGo.Text = "Go"
-    Me.btnGo.UseVisualStyleBackColor = True
-    '
-    'lbLog
-    '
-    Me.lbLog.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+        Me.btnGo.Location = New System.Drawing.Point(271, 12)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(317, 36)
+        Me.btnGo.TabIndex = 0
+        Me.btnGo.Text = "Go"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'lbLog
+        '
+        Me.lbLog.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-    Me.lbLog.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-    Me.lbLog.FormattingEnabled = True
-    Me.lbLog.IntegralHeight = False
-    Me.lbLog.ItemHeight = 14
-    Me.lbLog.Location = New System.Drawing.Point(12, 54)
-    Me.lbLog.Name = "lbLog"
-    Me.lbLog.ScrollAlwaysVisible = True
-    Me.lbLog.Size = New System.Drawing.Size(413, 196)
-    Me.lbLog.TabIndex = 1
-    '
-    'Button1
-    '
-    Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-    Me.Button1.Location = New System.Drawing.Point(337, 12)
-    Me.Button1.Name = "Button1"
-    Me.Button1.Size = New System.Drawing.Size(88, 36)
-    Me.Button1.TabIndex = 2
-    Me.Button1.Text = "Copy"
-    Me.Button1.UseVisualStyleBackColor = True
-    '
-    'MainForm
-    '
-    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-    Me.ClientSize = New System.Drawing.Size(437, 262)
-    Me.Controls.Add(Me.Button1)
-    Me.Controls.Add(Me.lbLog)
-    Me.Controls.Add(Me.btnGo)
-    Me.Name = "MainForm"
-    Me.Text = "PW EFA COM Test Version 1.1"
-    Me.ResumeLayout(False)
+        Me.lbLog.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbLog.FormattingEnabled = True
+        Me.lbLog.IntegralHeight = False
+        Me.lbLog.ItemHeight = 14
+        Me.lbLog.Location = New System.Drawing.Point(271, 54)
+        Me.lbLog.Name = "lbLog"
+        Me.lbLog.ScrollAlwaysVisible = True
+        Me.lbLog.Size = New System.Drawing.Size(411, 421)
+        Me.lbLog.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(594, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(88, 36)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Copy"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'pgMain
+        '
+        Me.pgMain.Location = New System.Drawing.Point(2, 12)
+        Me.pgMain.Name = "pgMain"
+        Me.pgMain.Size = New System.Drawing.Size(263, 463)
+        Me.pgMain.TabIndex = 3
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(694, 487)
+        Me.Controls.Add(Me.pgMain)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lbLog)
+        Me.Controls.Add(Me.btnGo)
+        Me.Name = "MainForm"
+        Me.Text = "PW EFA COM Test Version 1.1"
+        Me.ResumeLayout(False)
 
-  End Sub
-  Friend WithEvents btnGo As System.Windows.Forms.Button
+    End Sub
+    Friend WithEvents btnGo As System.Windows.Forms.Button
   Friend WithEvents lbLog As System.Windows.Forms.ListBox
   Friend WithEvents Button1 As System.Windows.Forms.Button
-
+    Friend WithEvents pgMain As PropertyGrid
 End Class
