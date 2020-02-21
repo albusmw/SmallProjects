2f472d8b77b9e77dea36d13aae3a7c6956ba90be
Martin Weiss <albusmw@gmx.de>
2020-02-21 16:35:51 +0100

    added TestBild project

---
+++ TestBild/TestBild/Form1.Designer.vb
@@ -0,0 +1,54 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pbTestImage = New System.Windows.Forms.PictureBox()
+        CType(Me.pbTestImage, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pbTestImage
+        '
+        Me.pbTestImage.BackColor = System.Drawing.Color.Black
+        Me.pbTestImage.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pbTestImage.Location = New System.Drawing.Point(0, 0)
+        Me.pbTestImage.Name = "pbTestImage"
+        Me.pbTestImage.Size = New System.Drawing.Size(800, 450)
+        Me.pbTestImage.TabIndex = 0
+        Me.pbTestImage.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.pbTestImage)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.pbTestImage, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pbTestImage As PictureBox
+End Class
---
+++ TestBild/TestBild/Form1.vb
@@ -0,0 +1,22 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class Form1
+
+    Private Sub pbTestImage_Click(sender As Object, e As EventArgs) Handles pbTestImage.Click
+
+        Dim R As Integer = 1
+        Dim R2 As Integer = 15
+        Dim PenToUse As New Pen(Color.White)
+
+        Dim g As Graphics = pbTestImage.CreateGraphics()
+        For X As Integer = 0 To pbTestImage.Width Step 50
+            For Y As Integer = 0 To pbTestImage.Height Step 50
+                g.DrawEllipse(PenToUse, New Rectangle(X - R, Y - R, 2 * R, R))
+                g.DrawEllipse(PenToUse, New Rectangle(X - R2, Y - R2, 2 * R2, 2 * R2))
+            Next Y
+        Next X
+
+    End Sub
+
+End Class
