e9a36774e22f6fd7dd5e06316b6ac449b7cf96dc
Martin Weiss <albusmw@gmx.de>
2020-09-15 22:05:06 +0200

    project AllSkyCapture added

---
+++ AllSkyCapture/Form1.Designer.vb
@@ -0,0 +1,279 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAscomCamera = New System.Windows.Forms.Button()
+        Me.btnTakeImage = New System.Windows.Forms.Button()
+        Me.pbLastImage = New System.Windows.Forms.PictureBox()
+        Me.tbLog = New System.Windows.Forms.TextBox()
+        Me.tbSelectedCam = New System.Windows.Forms.TextBox()
+        Me.tbExposeTime = New System.Windows.Forms.TextBox()
+        Me.btnOpenImage = New System.Windows.Forms.Button()
+        Me.scMain = New System.Windows.Forms.SplitContainer()
+        Me.tbStorageRoot = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnUp2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnUp10 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        CType(Me.pbLastImage, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.scMain, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.scMain.Panel1.SuspendLayout()
+        Me.scMain.Panel2.SuspendLayout()
+        Me.scMain.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnAscomCamera
+        '
+        Me.btnAscomCamera.Location = New System.Drawing.Point(12, 12)
+        Me.btnAscomCamera.Name = "btnAscomCamera"
+        Me.btnAscomCamera.Size = New System.Drawing.Size(114, 20)
+        Me.btnAscomCamera.TabIndex = 0
+        Me.btnAscomCamera.Text = "ASCOM Selector"
+        Me.btnAscomCamera.UseVisualStyleBackColor = True
+        '
+        'btnTakeImage
+        '
+        Me.btnTakeImage.Location = New System.Drawing.Point(298, 12)
+        Me.btnTakeImage.Name = "btnTakeImage"
+        Me.btnTakeImage.Size = New System.Drawing.Size(94, 20)
+        Me.btnTakeImage.TabIndex = 1
+        Me.btnTakeImage.Text = "Take image"
+        Me.btnTakeImage.UseVisualStyleBackColor = True
+        '
+        'pbLastImage
+        '
+        Me.pbLastImage.BackColor = System.Drawing.Color.Red
+        Me.pbLastImage.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pbLastImage.Location = New System.Drawing.Point(0, 0)
+        Me.pbLastImage.Name = "pbLastImage"
+        Me.pbLastImage.Size = New System.Drawing.Size(606, 627)
+        Me.pbLastImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pbLastImage.TabIndex = 2
+        Me.pbLastImage.TabStop = False
+        '
+        'tbLog
+        '
+        Me.tbLog.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.tbLog.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbLog.Location = New System.Drawing.Point(0, 0)
+        Me.tbLog.Multiline = True
+        Me.tbLog.Name = "tbLog"
+        Me.tbLog.ReadOnly = True
+        Me.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.tbLog.Size = New System.Drawing.Size(475, 627)
+        Me.tbLog.TabIndex = 3
+        '
+        'tbSelectedCam
+        '
+        Me.tbSelectedCam.Location = New System.Drawing.Point(132, 12)
+        Me.tbSelectedCam.Name = "tbSelectedCam"
+        Me.tbSelectedCam.Size = New System.Drawing.Size(160, 20)
+        Me.tbSelectedCam.TabIndex = 4
+        Me.tbSelectedCam.Text = "ASCOM.ASICamera2.Camera"
+        '
+        'tbExposeTime
+        '
+        Me.tbExposeTime.Location = New System.Drawing.Point(6, 19)
+        Me.tbExposeTime.Name = "tbExposeTime"
+        Me.tbExposeTime.Size = New System.Drawing.Size(63, 20)
+        Me.tbExposeTime.TabIndex = 5
+        Me.tbExposeTime.Text = "0.01"
+        Me.tbExposeTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'btnOpenImage
+        '
+        Me.btnOpenImage.Location = New System.Drawing.Point(298, 38)
+        Me.btnOpenImage.Name = "btnOpenImage"
+        Me.btnOpenImage.Size = New System.Drawing.Size(94, 20)
+        Me.btnOpenImage.TabIndex = 6
+        Me.btnOpenImage.Text = "Open image"
+        Me.btnOpenImage.UseVisualStyleBackColor = True
+        '
+        'scMain
+        '
+        Me.scMain.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.scMain.Location = New System.Drawing.Point(12, 94)
+        Me.scMain.Name = "scMain"
+        '
+        'scMain.Panel1
+        '
+        Me.scMain.Panel1.Controls.Add(Me.tbLog)
+        '
+        'scMain.Panel2
+        '
+        Me.scMain.Panel2.Controls.Add(Me.pbLastImage)
+        Me.scMain.Size = New System.Drawing.Size(1085, 627)
+        Me.scMain.SplitterDistance = 475
+        Me.scMain.TabIndex = 7
+        '
+        'tbStorageRoot
+        '
+        Me.tbStorageRoot.Location = New System.Drawing.Point(132, 38)
+        Me.tbStorageRoot.Name = "tbStorageRoot"
+        Me.tbStorageRoot.Size = New System.Drawing.Size(160, 20)
+        Me.tbStorageRoot.TabIndex = 10
+        Me.tbStorageRoot.Text = "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Storage root"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.btnUp2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.btnUp10)
+        Me.GroupBox1.Controls.Add(Me.tbExposeTime)
+        Me.GroupBox1.Location = New System.Drawing.Point(398, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(254, 46)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Exposure [s]"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(119, 17)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(38, 22)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "/ 2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'btnUp2
+        '
+        Me.btnUp2.Location = New System.Drawing.Point(161, 17)
+        Me.btnUp2.Name = "btnUp2"
+        Me.btnUp2.Size = New System.Drawing.Size(38, 22)
+        Me.btnUp2.TabIndex = 8
+        Me.btnUp2.Text = "x 2"
+        Me.btnUp2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(75, 17)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(38, 22)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "/ 10"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnUp10
+        '
+        Me.btnUp10.Location = New System.Drawing.Point(205, 17)
+        Me.btnUp10.Name = "btnUp10"
+        Me.btnUp10.Size = New System.Drawing.Size(38, 22)
+        Me.btnUp10.TabIndex = 6
+        Me.btnUp10.Text = "x 10"
+        Me.btnUp10.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(658, 11)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(69, 47)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "Gain MAX"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(733, 11)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(69, 47)
+        Me.Button4.TabIndex = 14
+        Me.Button4.Text = "Gain MIN"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(808, 12)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(94, 46)
+        Me.Button5.TabIndex = 15
+        Me.Button5.Text = "Test FTP"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1109, 733)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.tbStorageRoot)
+        Me.Controls.Add(Me.scMain)
+        Me.Controls.Add(Me.btnOpenImage)
+        Me.Controls.Add(Me.tbSelectedCam)
+        Me.Controls.Add(Me.btnTakeImage)
+        Me.Controls.Add(Me.btnAscomCamera)
+        Me.Name = "Form1"
+        Me.Text = "AllSkyCapture Version 1.1 (21.11.2017)"
+        CType(Me.pbLastImage, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.scMain.Panel1.ResumeLayout(False)
+        Me.scMain.Panel1.PerformLayout()
+        Me.scMain.Panel2.ResumeLayout(False)
+        CType(Me.scMain, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.scMain.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnAscomCamera As System.Windows.Forms.Button
+    Friend WithEvents btnTakeImage As System.Windows.Forms.Button
+    Friend WithEvents pbLastImage As System.Windows.Forms.PictureBox
+    Friend WithEvents tbLog As System.Windows.Forms.TextBox
+    Friend WithEvents tbSelectedCam As System.Windows.Forms.TextBox
+    Friend WithEvents tbExposeTime As System.Windows.Forms.TextBox
+    Friend WithEvents btnOpenImage As System.Windows.Forms.Button
+    Friend WithEvents scMain As SplitContainer
+    Friend WithEvents tbStorageRoot As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnUp10 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents btnUp2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+End Class
---
+++ AllSkyCapture/Form1.vb
@@ -0,0 +1,264 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class Form1
+
+    Const GainNotSet As Short = Short.MinValue + 1
+
+    Dim SelectedCamera As String = "ASCOM.ASICamera2.Camera"
+    Dim FileName As String
+    Dim LastLoggedDate As New DateTime(0)
+    Dim SelectedGain As Short = GainNotSet
+    Dim CurrentImageName As String = "AllSkyImage"
+
+    Private Sub btnAscomCamera_Click(sender As Object, e As EventArgs) Handles btnAscomCamera.Click
+        tbSelectedCam.Text = Chooser_Camera()
+        Log("Selected: " & tbSelectedCam.Text)
+    End Sub
+
+    Private Function Chooser_Camera() As String
+        Dim Chooser As New ASCOM.Utilities.Chooser
+        Chooser.DeviceType = "Camera"
+        SelectedCamera = Chooser.Choose(SelectedCamera)
+        Return SelectedCamera
+    End Function
+
+    Private Sub btnTakeImage_Click(sender As Object, e As EventArgs) Handles btnTakeImage.Click
+
+        Log("Connecting camera")
+        Dim Camera As New ASCOM.DriverAccess.Camera(SelectedCamera)
+        Camera.Connected = True
+        Log("    DONE (" & Camera.Description & ")")
+        Log("    Gain range    : " & Camera.GainMin.ToString.Trim & " ... " & Camera.GainMax.ToString.Trim)
+        Log("    Exposure range: " & Camera.ExposureMin.ToString.Trim & " ... " & Camera.ExposureMax.ToString.Trim)
+
+        Camera.BinX = 1
+        Camera.BinY = 1
+
+        'Set gain
+        If SelectedGain <> GainNotSet Then
+            If SelectedGain > Camera.GainMax Then SelectedGain = Camera.GainMax
+            If SelectedGain < Camera.GainMin Then SelectedGain = Camera.GainMin
+            Camera.Gain = SelectedGain
+        End If
+        Log("    Gain selected: " & Camera.Gain.ToString.Trim)
+
+        StartLog("Starting exposure with " & tbExposeTime.Text.ToString.Trim & " seconds")
+        btnTakeImage.Enabled = False : System.Windows.Forms.Application.DoEvents()
+        Camera.StartExposure(Val(tbExposeTime.Text.Replace(",", ".")), False)
+        Do
+            If Camera.ImageReady Then Exit Do
+        Loop Until 1 = 0
+        Dim CCDSensorData As Integer(,) = CType(Camera.ImageArray, Integer(,))
+        btnTakeImage.Enabled = True : System.Windows.Forms.Application.DoEvents()
+        FinishLog("    DONE.")
+
+        Dim PixelX As Integer = CCDSensorData.GetUpperBound(0) + 1
+        Dim PixelY As Integer = CCDSensorData.GetUpperBound(1) + 1
+
+        Log(PixelX.ToString.Trim & " x " & PixelY.ToString.Trim & " = " & (PixelX * PixelY).ToString.Trim & ", " & Camera.SensorType.ToString)
+
+        Dim BitmapToCreate As Bitmap
+        If Camera.SensorType <> ASCOM.DeviceInterface.SensorType.Monochrome Then
+            BitmapToCreate = New Bitmap((CCDSensorData.GetUpperBound(0) + 1) \ 2, (CCDSensorData.GetUpperBound(1) + 1) \ 2, System.Drawing.Imaging.PixelFormat.Format24bppRgb)      'color mode
+        Else
+            BitmapToCreate = New Bitmap((CCDSensorData.GetUpperBound(0) + 1), (CCDSensorData.GetUpperBound(1) + 1), System.Drawing.Imaging.PixelFormat.Format8bppIndexed)              'grayscale mode (we use 24bppRgb as well here - to be improved ...)
+        End If
+
+        'Prepare underlaying bitmap byte data and copy 
+        Dim BitmapByteData As System.Drawing.Imaging.BitmapData = BitmapToCreate.LockBits(New Rectangle(0, 0, BitmapToCreate.Width, BitmapToCreate.Height), Drawing.Imaging.ImageLockMode.ReadWrite, BitmapToCreate.PixelFormat)
+        Dim FirstLineAdr As IntPtr = BitmapByteData.Scan0
+        Dim TotalByteCount As Integer = Math.Abs(BitmapByteData.Stride) * BitmapToCreate.Height
+        Log(TotalByteCount.ToString.Trim & " byte for bitmap")
+
+        Dim RGBValues(TotalByteCount - 1) As Byte
+        System.Runtime.InteropServices.Marshal.Copy(FirstLineAdr, RGBValues, 0, TotalByteCount)
+
+        'Start manipulation of the image - copy CCD data data from the camera to the image structure
+        Dim CCDSensorBayerStep As Integer = 0
+        Dim BytePerOutputPixel As Integer = 0      'RGB, each 1 byte
+        Dim Scaler As Integer = 256
+        Dim Min As Integer = Integer.MaxValue
+        Dim Max As Integer = Integer.MinValue
+        Dim Y As Integer = 0
+        Dim X As Integer = 0
+        If Camera.SensorType <> ASCOM.DeviceInterface.SensorType.Monochrome Then
+            'Color sensor
+            CCDSensorBayerStep = 2
+            BytePerOutputPixel = 3       'RGB, each 1 byte
+            For Counter As Integer = 0 To RGBValues.Length - 1 Step BytePerOutputPixel
+                'Read the bayer matrix data (only use 3 of 4 pixel)
+                Dim R As Byte = CByte(Math.Floor(CCDSensorData(Y + 1, X) / Scaler))
+                Dim G As Byte = CByte(Math.Floor(CCDSensorData(Y, X) / Scaler))
+                Dim B As Byte = CByte(Math.Floor(CCDSensorData(Y, X + 1) / Scaler))
+                'Transfer the data to the byte array
+                RGBValues(Counter) = B
+                RGBValues(Counter + 1) = R
+                RGBValues(Counter + 2) = G
+                Y += CCDSensorBayerStep
+                If Y > CCDSensorData.GetUpperBound(0) Then
+                    Y = 0
+                    X += CCDSensorBayerStep
+                End If
+            Next Counter
+        Else
+            'Monochromatic sensor
+            CCDSensorBayerStep = 1
+            BytePerOutputPixel = 1
+            For Counter As Integer = 0 To RGBValues.Length - 1 Step BytePerOutputPixel
+                'Read the grayscale data
+                Dim Value As Byte = CByte(Math.Floor(CCDSensorData(Y, X) / Scaler))
+                'Transfer the data to the byte array
+                RGBValues(Counter) = Value
+                'RGBValues(Counter + 1) = Value
+                'RGBValues(Counter + 2) = Value
+                Y += CCDSensorBayerStep
+                If Y > CCDSensorData.GetUpperBound(0) Then
+                    Y = 0
+                    X += CCDSensorBayerStep
+                End If
+            Next Counter
+        End If
+
+        System.Runtime.InteropServices.Marshal.Copy(RGBValues, 0, FirstLineAdr, TotalByteCount)
+        BitmapToCreate.UnlockBits(BitmapByteData)
+
+        'Create a grayscale palette
+        If Camera.SensorType = ASCOM.DeviceInterface.SensorType.Monochrome Then
+            Dim GrayScale As Imaging.ColorPalette = BitmapToCreate.Palette
+            For Idx As Integer = 0 To 255
+                GrayScale.Entries(Idx) = Color.FromArgb(Idx, Idx, Idx)
+            Next Idx
+            BitmapToCreate.Palette = GrayScale
+        End If
+
+        'Set the image to be displayed
+        pbLastImage.Image = BitmapToCreate
+
+        'Save image
+        Dim BaseFileName As String = CurrentImageName
+        FileName = tbStorageRoot.Text & "\" & BaseFileName
+        StartLog("Saving image to <" & BaseFileName & ".png" & ">")
+        BitmapToCreate.Save(FileName & ".png", System.Drawing.Imaging.ImageFormat.Png)
+        StartLog("Saving image to <" & BaseFileName & ".jpg" & ">")
+        BitmapToCreate.Save(FileName & ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg)
+        FinishLog("    DONE.")
+
+        StartLog("Disconnecting camera")
+        Camera.Connected = False
+        FinishLog("    DONE.")
+        Log("==============================================")
+
+    End Sub
+
+    Private Sub Log(ByVal NewText As String)
+        StartLog(NewText)
+        FinishLog(String.Empty)
+    End Sub
+
+    Private Sub StartLog(ByVal NewText As String)
+        ShowNewDate()
+        'Log message
+        With tbLog
+            .Text &= Format(Now, "HH:mm:ss") & "|" & NewText
+            .SelectionStart = .Text.Length
+            .ScrollToCaret()
+        End With
+    End Sub
+
+    Private Sub FinishLog(ByVal NewText As String)
+        'Log message
+        With tbLog
+            .Text &= NewText & System.Environment.NewLine
+            .SelectionStart = .Text.Length
+            .ScrollToCaret()
+        End With
+    End Sub
+
+    Private Function ShowNewDate() As Boolean
+        'Log date if date is changed
+        Dim CurrentDate As DateTime = Now
+        If LastLoggedDate.Year <> CurrentDate.Year Or LastLoggedDate.Month <> CurrentDate.Month Or LastLoggedDate.Day <> CurrentDate.Day Then
+            tbLog.Text &= "##########################" & System.Environment.NewLine & Format(CurrentDate, "yyyy-MM-dd") & System.Environment.NewLine & "##########################" & System.Environment.NewLine
+            LastLoggedDate = CurrentDate
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Sub btnOpenImage_Click(sender As Object, e As EventArgs) Handles btnOpenImage.Click
+        If MsgBox("YES for JPEG, NO for PNG", MsgBoxStyle.YesNo Or MsgBoxStyle.Question) = MsgBoxResult.Yes Then
+            Process.Start(FileName & ".jpg")
+        Else
+            Process.Start(FileName & ".png")
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tbStorageRoot.Text = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)
+    End Sub
+
+    Private Sub btnUp10_Click(sender As Object, e As EventArgs) Handles btnUp10.Click
+        tbExposeTime.Text = CStr(Val(tbExposeTime.Text) * 10).Trim.Replace(",", ".")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        tbExposeTime.Text = CStr(Val(tbExposeTime.Text) / 10).Trim.Replace(",", ".")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        tbExposeTime.Text = CStr(Val(tbExposeTime.Text) / 2).Trim.Replace(",", ".")
+    End Sub
+
+    Private Sub btnUp2_Click(sender As Object, e As EventArgs) Handles btnUp2.Click
+        tbExposeTime.Text = CStr(Val(tbExposeTime.Text) * 2).Trim.Replace(",", ".")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        SelectedGain = Short.MaxValue
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        SelectedGain = Short.MinValue
+    End Sub
+
+    Private Sub UploadFile(ByVal File As String)
+
+        'File generated: http://www.albusmw.de/transfer/AllSkyImage.jpg
+
+        Dim FTP_Server As String = "ftp.strato.de"
+        Dim FTP_User As String = "ftp_AllSky@albusmw.de"
+        Dim FTP_Pwd As String = "qbZHD57224"
+
+        'Get the object used to communicate with the server.  
+        Dim RemoteFilename As String = "ftp://www.albusmw.de/" & CurrentImageName & ".jpg"
+        Dim request As Net.FtpWebRequest = CType(Net.WebRequest.Create(RemoteFilename), Net.FtpWebRequest)
+        request.Method = Net.WebRequestMethods.Ftp.UploadFile
+
+        'This example assumes the FTP site uses anonymous logon.  
+        request.Credentials = New Net.NetworkCredential(FTP_User, FTP_Pwd)
+
+        'Copy the contents of the file to the request stream.  
+        Dim sourceStream As New IO.StreamReader(File)
+        Dim fileContents As Byte() = System.Text.Encoding.UTF8.GetBytes(sourceStream.ReadToEnd())
+        sourceStream.Close()
+        request.ContentLength = fileContents.Length
+
+        Dim requestStream As IO.Stream = request.GetRequestStream()
+        requestStream.Write(fileContents, 0, fileContents.Length)
+        requestStream.Close()
+
+        Dim response As Net.FtpWebResponse = CType(request.GetResponse(), Net.FtpWebResponse)
+
+        Console.WriteLine("Upload File Complete, status {0}", response.StatusDescription)
+
+        response.Close()
+
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        UploadFile(CurrentImageName & ".jpg")
+    End Sub
+End Class
