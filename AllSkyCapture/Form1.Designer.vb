a1d207c53888041fd0cbf3e01233474ede1a872a
albusmw <albusmw@gmx.de>
2021-02-07 14:30:56 +0100

    minor change on AllSkyCapture

--- AllSkyCapture/Form1.Designer.vb
+++ AllSkyCapture/Form1.Designer.vb
@@ -41,6 +41,7 @@ Partial Class Form1
         Me.tsmiTakeOnePicture = New System.Windows.Forms.ToolStripMenuItem()
         Me.ToolStripMenuItem3 = New System.Windows.Forms.ToolStripSeparator()
         Me.tsmiFTPUpload = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TelnetClientToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.SetToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.MyCaptureDefaultsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.tCheckExpState = New System.Windows.Forms.Timer(Me.components)
@@ -48,7 +49,7 @@ Partial Class Form1
         Me.tsslCapture = New System.Windows.Forms.ToolStripStatusLabel()
         Me.tsslSunPos = New System.Windows.Forms.ToolStripStatusLabel()
         Me.tsslNoCapture = New System.Windows.Forms.ToolStripStatusLabel()
-        Me.TelnetClientToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.tsmiFile_ClearLog = New System.Windows.Forms.ToolStripMenuItem()
         CType(Me.pbLastImage, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.scMain, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.scMain.Panel1.SuspendLayout()
@@ -143,7 +144,7 @@ Partial Class Form1
         '
         'FileToolStripMenuItem
         '
-        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SelectCameraToolStripMenuItem, Me.ToolStripMenuItem2, Me.OpenLastImageToolStripMenuItem, Me.OpenStoragePathToolStripMenuItem, Me.JoinToVideoToolStripMenuItem, Me.ToolStripMenuItem1, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SelectCameraToolStripMenuItem, Me.ToolStripMenuItem2, Me.OpenLastImageToolStripMenuItem, Me.OpenStoragePathToolStripMenuItem, Me.JoinToVideoToolStripMenuItem, Me.ToolStripMenuItem1, Me.tsmiFile_ClearLog, Me.ExitToolStripMenuItem})
         Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
         Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
         Me.FileToolStripMenuItem.Text = "File"
@@ -151,41 +152,41 @@ Partial Class Form1
         'SelectCameraToolStripMenuItem
         '
         Me.SelectCameraToolStripMenuItem.Name = "SelectCameraToolStripMenuItem"
-        Me.SelectCameraToolStripMenuItem.Size = New System.Drawing.Size(172, 22)
+        Me.SelectCameraToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.SelectCameraToolStripMenuItem.Text = "Select camera"
         '
         'ToolStripMenuItem2
         '
         Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
-        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(169, 6)
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(177, 6)
         '
         'OpenLastImageToolStripMenuItem
         '
         Me.OpenLastImageToolStripMenuItem.Name = "OpenLastImageToolStripMenuItem"
-        Me.OpenLastImageToolStripMenuItem.Size = New System.Drawing.Size(172, 22)
+        Me.OpenLastImageToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.OpenLastImageToolStripMenuItem.Text = "Open last image"
         '
         'OpenStoragePathToolStripMenuItem
         '
         Me.OpenStoragePathToolStripMenuItem.Name = "OpenStoragePathToolStripMenuItem"
-        Me.OpenStoragePathToolStripMenuItem.Size = New System.Drawing.Size(172, 22)
+        Me.OpenStoragePathToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.OpenStoragePathToolStripMenuItem.Text = "Open storage path"
         '
         'JoinToVideoToolStripMenuItem
         '
         Me.JoinToVideoToolStripMenuItem.Name = "JoinToVideoToolStripMenuItem"
-        Me.JoinToVideoToolStripMenuItem.Size = New System.Drawing.Size(172, 22)
+        Me.JoinToVideoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.JoinToVideoToolStripMenuItem.Text = "Join to video"
         '
         'ToolStripMenuItem1
         '
         Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
-        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(169, 6)
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(177, 6)
         '
         'ExitToolStripMenuItem
         '
         Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
-        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(172, 22)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.ExitToolStripMenuItem.Text = "Exit"
         '
         'TestToolStripMenuItem
@@ -212,6 +213,12 @@ Partial Class Form1
         Me.tsmiFTPUpload.Size = New System.Drawing.Size(180, 22)
         Me.tsmiFTPUpload.Text = "FTP upload"
         '
+        'TelnetClientToolStripMenuItem
+        '
+        Me.TelnetClientToolStripMenuItem.Name = "TelnetClientToolStripMenuItem"
+        Me.TelnetClientToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.TelnetClientToolStripMenuItem.Text = "Telnet client"
+        '
         'SetToolStripMenuItem
         '
         Me.SetToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MyCaptureDefaultsToolStripMenuItem})
@@ -222,7 +229,7 @@ Partial Class Form1
         'MyCaptureDefaultsToolStripMenuItem
         '
         Me.MyCaptureDefaultsToolStripMenuItem.Name = "MyCaptureDefaultsToolStripMenuItem"
-        Me.MyCaptureDefaultsToolStripMenuItem.Size = New System.Drawing.Size(179, 22)
+        Me.MyCaptureDefaultsToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
         Me.MyCaptureDefaultsToolStripMenuItem.Text = "My capture defaults"
         '
         'tCheckExpState
@@ -257,11 +264,11 @@ Partial Class Form1
         Me.tsslNoCapture.Size = New System.Drawing.Size(119, 17)
         Me.tsslNoCapture.Text = "ToolStripStatusLabel1"
         '
-        'TelnetClientToolStripMenuItem
+        'tsmiFile_ClearLog
         '
-        Me.TelnetClientToolStripMenuItem.Name = "TelnetClientToolStripMenuItem"
-        Me.TelnetClientToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
-        Me.TelnetClientToolStripMenuItem.Text = "Telnet client"
+        Me.tsmiFile_ClearLog.Name = "tsmiFile_ClearLog"
+        Me.tsmiFile_ClearLog.Size = New System.Drawing.Size(180, 22)
+        Me.tsmiFile_ClearLog.Text = "Clear log"
         '
         'Form1
         '
@@ -318,4 +325,5 @@ Partial Class Form1
     Friend WithEvents tsslCapture As ToolStripStatusLabel
     Friend WithEvents tsslNoCapture As ToolStripStatusLabel
     Friend WithEvents TelnetClientToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents tsmiFile_ClearLog As ToolStripMenuItem
 End Class
--- AllSkyCapture/Form1.vb
+++ AllSkyCapture/Form1.vb
@@ -425,4 +425,8 @@ Public Class Form1
         TakeImage(Never)
     End Sub
 
+    Private Sub tsmiFile_ClearLog_Click(sender As Object, e As EventArgs) Handles tsmiFile_ClearLog.Click
+        tbLog.Text = String.Empty
+    End Sub
+
 End Class
